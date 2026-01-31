91134313af6a5065240bc14e4547dd2bd4c81d4d
XVSHIFU <1878368767@qq.com>
2026-01-31 20:48:14 +0800

    Update imports and maintain functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 import os
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -115,3 +115,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
